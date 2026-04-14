6f70b4a29587be29c04b2fefc1862a297bae72f1
Anthony Xavier <anthonyltx12@gmail.com>
2026-04-14 20:02:31 -0300

    feat(estrutura): Adicionado pastas e dependencias para a esttrutura do projeto

---
+++ src/main/java/com/projetotematico/SimulaInvest/SimulaInvestApplication.java
@@ -0,0 +1,13 @@
+package com.projetotematico.SimulaInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulaInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulaInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetotematico/SimulaInvest/SimulaInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetotematico.SimulaInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulaInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
