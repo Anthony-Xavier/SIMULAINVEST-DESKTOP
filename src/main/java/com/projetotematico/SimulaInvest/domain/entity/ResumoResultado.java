32cacb5b1516fda5135b2e8f6f2b676eea0940f5
Anthony Xavier <anthonyltx12@gmail.com>
2026-05-05 19:56:34 -0300

    fix(service):tinha esquecido de colocar o getter e o setter

--- src/main/java/com/projetotematico/SimulaInvest/domain/entity/ResumoResultado.java
+++ src/main/java/com/projetotematico/SimulaInvest/domain/entity/ResumoResultado.java
@@ -1,7 +1,15 @@
 package com.projetotematico.SimulaInvest.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "resumos_resultado")
 public class ResumoResultado {
